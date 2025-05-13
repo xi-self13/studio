5de7d5038ca6324f8c45fecc29b8d6c41eacff55
Ronreacus Adams <ronreacusadams12@gmail.com>
2025-05-13 18:03:21 +0000

    undo

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // Added for static HTML export
   typescript: {
     ignoreBuildErrors: true,
   },
